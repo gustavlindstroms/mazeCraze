4ee51ef7b94f1ed2ea7f04323709d761ac9d4718
Gustav Lindström <44166712+gustavlindstroms@users.noreply.github.com>
2019-12-15 18:00:07 +0100

    updated drawWindow

--- drawWindow.py
+++ drawWindow.py
@@ -1,6 +1,61 @@
+
 import wx
 
-app = wx.App()
-frame = wx.Frame(parent=None, title='Hello World')
-frame.Show()
-app.MainLoop()
+class Example(wx.Frame):
+
+    def __init__(self, *args, **kw):
+        super(Example, self).__init__(*args, **kw)
+
+        self.InitUI()
+
+    def InitUI(self):
+
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+
+        self.SetTitle("Colours")
+        self.Centre()
+
+
+    def OnPaint(self, e):
+
+        dc = wx.PaintDC(self)
+        dc.SetPen(wx.Pen('#000000'))
+
+        dc.SetBrush(wx.Brush('#000000'))
+        dc.DrawRectangle(10, 15, 90, 60)
+
+        dc.SetBrush(wx.Brush('#FFFFFF'))
+        dc.DrawRectangle(130, 15, 90, 60)
+
+        dc.SetBrush(wx.Brush('#000000'))
+        dc.DrawRectangle(250, 15, 90, 60)
+
+        dc.SetBrush(wx.Brush('#000000'))
+        dc.DrawRectangle(10, 105, 90, 60)
+
+        dc.SetBrush(wx.Brush('#FFFFFF'))
+        dc.DrawRectangle(130, 105, 90, 60)
+
+        dc.SetBrush(wx.Brush('#000000'))
+        dc.DrawRectangle(250, 105, 90, 60)
+
+        dc.SetBrush(wx.Brush('#000000'))
+        dc.DrawRectangle(10, 195, 90, 60)
+
+        dc.SetBrush(wx.Brush('#FFFFFF'))
+        dc.DrawRectangle(130, 195, 90, 60)
+
+        dc.SetBrush(wx.Brush('#000000'))
+        dc.DrawRectangle(250, 195, 90, 60)
+
+
+def main():
+
+    app = wx.App()
+    ex = Example(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
