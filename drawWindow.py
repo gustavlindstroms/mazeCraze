4596daedb3d55004c538038b2652021f4f5b674c
gustavlindstroms <44166712+gustavlindstroms@users.noreply.github.com>
2019-12-22 19:53:54 +0100

    updated maze and draw function

--- drawWindow.py
+++ drawWindow.py
@@ -1,22 +1,16 @@
 import csv
-import wx
-import tkinter
-import mazecreator
-from tkinter import Tk, Canvas, Frame, BOTH
+from tkinter import Tk, Canvas, Frame, BOTH, Button
+arr = []
 
 class Example(Frame):
 
     def __init__(self):
         super().__init__()
-
         self.initUI()
 
-
     def initUI(self):
-
-        self.master.title("Shapes")
+        self.master.title("MazeCraze")
         self.pack(fill=BOTH, expand=1)
-
         canvas = Canvas(self)
         with open('C:/Users/gusta/OneDrive/Documents/Plenissalen/mazeCraze/maze.csv') as f:
             reader = csv.reader(f)
@@ -32,35 +26,33 @@ class Example(Frame):
             minor = i
             for x in minor:
                 print(x)
-                color = ''
                 if x == '1':
-                    color = '#000'
+                    color = 'gray15'
+                elif x == '3':
+                    color = 'red'
                 else:
-                    color = '#fff'
-
+                    color = 'white'
                 if rowCounter == size:
                     yAxis += 10
                     rowCounter = 0
                     xAxis=0
-                canvas.create_rectangle(xAxis, yAxis, xAxis+10, yAxis+10, outline=color, fill=color, width=2)
-
-                print('printed one with color ' + color)
-                color = ''
+                rectangle = canvas.create_rectangle(xAxis, yAxis, xAxis+10, yAxis+10, outline=color, fill=color, width=2)
+                arr.append(rectangle)
                 xAxis += 10
                 rowCounter += 1
 
         canvas.pack(fill=BOTH, expand=1)
+        button = Button(self, text='Solve', command=printHello)
+        button.pack(fill=BOTH, expand=1)
 
+def printHello():
+    print('Hello')
 
 def main():
-
     root = Tk()
     ex = Example()
     root.geometry("330x220+300+300")
     root.mainloop()
 
-
 if __name__ == '__main__':
     main()
-
-
